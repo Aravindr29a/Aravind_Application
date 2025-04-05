57705407675429d59d48f21a46950d544783124c
Aravindr29a <swamyaravind591@gmail.com>
2025-04-05 17:49:04 +0530

    Initial commit

---
+++ Entrylevel.java
@@ -0,0 +1,40 @@
+package com.apra;
+
+public class Entrylevel {
+
+	public static void main(String[] args) {
+		namePrint();
+		space();
+		cityName();
+		
+		
+	}
+	
+	public static void namePrint() {
+		System.out.println("R Aravind");
+	}
+	
+	public static void cityName() {
+		System.out.println("Chennai");
+	}
+	
+	public static void space() { 
+		System.out.println(" ");
+		
+		}
+
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
